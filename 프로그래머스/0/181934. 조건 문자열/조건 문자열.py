071b50103fb6d3a9c5d977d1cbdd18f8941061f0
parkminha76 <parkminha76@gmail.com>
2026-09-02 15:35:59 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,13 @@
+def solution(ineq, eq, n, m):
+    answer = 0
+    
+    if n >= m:
+        return 1
+    elif n <= m:
+        return 1
+    elif n != m:
+        return 0
+    else :
+        return 0
+        
+    return answer
